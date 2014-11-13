fea910f0ecb8d38ab1f87267d92927d0eaa511a1
Gayan Virajith <gayanvirajith@gmail.com>
2014-11-13 10:07:33 +0530

    Modified call by reference program-010

--- prog-010/prog-10.m
+++ prog-010/prog-10.m
@@ -28,6 +28,21 @@ int main(int argc, const char *argv[]) {
 
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
+	// Local variable definition
+	int a = 100;
+	int b = 200;
+
+	SampleClass *sampleClass = [[SampleClass alloc] init];
+
+	NSLog(@ "Before swap, value of a : %d", a );
+	NSLog(@ "Before swap, value of b : %d", b );
+
+	// Remember to put `&` before the argument
+	[sampleClass swap:&a andNum2:&b];
+
+	NSLog(@ "After swap, value of a : %d", a );
+	NSLog(@ "After swap, value of b : %d", b );
+
 	[pool drain];
 
 	return 0;
